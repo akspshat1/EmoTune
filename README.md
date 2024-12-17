13714127f28c69b73c4ab14172af2ca6ae16f5f3
Akshat <akshatkhaptya@gmail.com>
2024-12-17 09:18:15 +0530

    All commit: Uploading project files

---
+++ README.md
@@ -0,0 +1,39 @@
+# Emotion-Based Music Player
+
+## Overview
+
+The Emotion-Based Music Player EMO-TUNE is a unique application designed to enhance your music listening experience by curating playlists based on your current emotional state. Whether you're feeling happy, sad, energized, or relaxed, this music player selects tracks that match your mood, helping you immerse yourself in the right atmosphere.
+
+## Features
+
+- **Emotion Detection**: Utilizes advanced algorithms based on CNN to analyze your emotional state based on facial expression.
+- **Intuitive Interface**: User-friendly interface makes it easy to navigate.
+- **Customizable Playlists**: Curates personalized playlists tailored to your emotions, ensuring that the music aligns perfectly with how you feel.
+- **Real-time Updates**: Continuously updates playlists as your emotional state changes, providing a seamless listening experience.
+- **Extensive Music Library**: Access a vast collection of songs across different genres and moods to suit every occasion.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/RHYTHM2405/EmoTune`
+2. Navigate to the project directory: `cd codes`
+3. Install dependencies: `npm install`
+4. Start the application: `python streamlit.py`
+
+## Usage
+
+1. Launch the Emotion-Based Music Player application.
+2. It detects current emotional state based on the facial expressions.
+3. Sit back, relax, and enjoy a curated playlist that matches your mood.
+4. Explore additional features such as favorite tracks, shuffle mode, and more.
+
+## Support
+
+If you encounter any issues or have suggestions for improvement, please feel free to reach out to our support team at b22cs042@iitj.ac.in , b22cs098@iitj.ac.in . We're here to help!
+
+## Contributing
+
+We welcome contributions from the community to enhance the EmoTune App. To contribute, please fork the repository, make your changes, and submit a pull request. Together, we can make music listening a truly immersive experience!
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ streamlit_app.py
@@ -0,0 +1,109 @@
+from rmn import RMN
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+from collections import Counter
+import random
+import os
+import threading
+
+
+
+
+model = RMN()
+
+
+emotion_folders = {
+    "angry": r"songs\calm",
+    "happy": r"songs\happy",
+    "surprise": r"songs\surprise",
+    "sad": r"songs\sad",
+    "calm": r"songs\calm",
+}
+
+emotion_css_files = {
+        "happy": "happy.css",
+        "sad": "sad.css",
+        "calm":"calm.css",
+        "angry":"angry.css",
+        "surprise":"surprise.css"
+
+}
+
+
+
+
+
+def process_image(image):
+    image = np.array(image)
+    emotion = model.detect_emotion_for_single_frame(image)
+    return emotion
+
+def capture_face():
+    capture_duration = 1  # Capture images for 5 seconds
+    cap = cv2.VideoCapture(0)  # Open the camera (0 for default camera)
+
+    images = []
+    start_time = cv2.getTickCount()
+
+    while True:
+        ret, frame = cap.read()  # Read a frame from the camera
+        if not ret:
+            break
+
+        # Convert the frame to RGB format
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        images.append(Image.fromarray(rgb_frame))
+        #st.image(rgb_frame, channels="RGB", use_column_width=True, caption="Camera Capture")
+        # Check if capture duration is reached
+        elapsed_time = (cv2.getTickCount() - start_time) / cv2.getTickFrequency()
+        if elapsed_time >= capture_duration:
+            break
+
+    cap.release()  # Release the camera
+    
+    emotions = []
+    for image in images:
+        emotions.append(process_image(image))
+    emotion_counts = Counter(emotions)
+    most_common_emotion = emotion_counts.most_common(1)[0][0]
+    
+    return most_common_emotion
+
+
+
+def main():
+    st.title("EmoTune")
+    # st.markdown("Tune Your Experience")
+    st.markdown("Capture your emotion and enjoy music tailored to your mood.")
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+   
+    curr_emo = None
+    
+    if st.button("Play"):
+        with st.spinner("Detecting emotion..."):
+            curr_emo = capture_face()
+            
+        st.write("Your Current emotion is:", curr_emo)
+        if curr_emo in emotion_css_files:
+            css_file = emotion_css_files[curr_emo]
+            with open(css_file) as f:
+                st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)      
+        # Define path to your songs folder based on current emotion
+        songs_emo = emotion_folders.get(curr_emo)  # Get the path based on current emotion
+        if songs_emo is not None:
+            random_num = np.random.randint(1, 3)
+            song_filename = f"{random_num}.mp3"  # Assuming your songs are named like "song1.mp3", "song2.mp3", etc.
+            song_path = os.path.join(songs_emo, song_filename)  # Joining the folder path with the random song filename
+            st.audio(song_path, format="audio/mp3", loop=True)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
